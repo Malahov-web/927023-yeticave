695aed2daff0ae43879041ddb86502cad009255a
Kirill Malakhov <malahovk@gmail.com>
2018-11-28 19:06:30 +0400

    #9 Исправил по замечаниям

--- schema.sql
+++ schema.sql
@@ -4,47 +4,46 @@
 
 USE yeticave;
 
-CREATE TABLE lots (
+CREATE TABLE lot (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	title CHAR,
-	descr CHAR,
-	img CHAR,
-	price_start INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	title CHAR NOT NULL,
+	description CHAR,
+	img_url CHAR NOT NULL,
+	price_start INT NOT NULL,
     end_date DATETIME,
 	bet_step INT,
-	cat_id INT,
-	user_id INT,
-	winner_id INT
+	cat_id INT references category(id),
+	user_id INT references user(id),
+	winner_id INT references user(id)
 );
 
-CREATE TABLE categories(
+CREATE TABLE category(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	title CHAR
+	title CHAR,
+    UNIQUE INDEX index_category (title)
 );
 
-CREATE TABLE users (
+CREATE TABLE user(
     id INT AUTO_INCREMENT PRIMARY KEY,
-	add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     email CHAR(128) NOT NULL UNIQUE,
 	name CHAR(128),
     pass CHAR(64) NOT NULL UNIQUE,
-    avatar CHAR,
+    avatar_url CHAR NOT NULL,
     contacts TEXT,
-    lots_id INT,
-    bets_id INT
+    lots_id INT references lot(id),
+    bets_id INT references bet(id),
+    UNIQUE INDEX index_user (email, pass)
 );
 
-CREATE TABLE bets(
+CREATE TABLE bet(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	add_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     bet_value INT,
-    user_id INT,
-    lot_id INT
+    user_id INT references user(id),
+    lot_id INT references lot(id)
 );
 
-CREATE INDEX l_title ON lots(title);
-CREATE INDEX l_descr ON lots(descr);
-CREATE UNIQUE INDEX c_title ON categories(title);
-CREATE UNIQUE INDEX u_email ON users(email);
-CREATE UNIQUE INDEX u_pass ON users(pass);
+CREATE INDEX index_title ON lot(title);
+CREATE INDEX index_description ON lot(description);
