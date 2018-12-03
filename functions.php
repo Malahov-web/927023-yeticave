a4abda51efec44ce41e20ea88f6f1bc091754e36
Kirill Malakhov <malahovk@gmail.com>
2018-12-03 20:39:12 +0400

    Форматирование кода

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function include_template($name, $data) {
+function include_template($name, $data)
+{
     $name = 'templates/' . $name;
     $result = '';
 
@@ -18,26 +19,28 @@ function include_template($name, $data) {
 }
 
 
-function format_price( float $price ): string {
+function format_price(float $price): string
+{
     $price_rounded = ceil($price);
 
-    if ( $price_rounded >= 1000 ){
-        $price_string = (string) $price_rounded;
-        $price_start  = substr($price_string, 0, strlen($price_string)-3);
+    if ($price_rounded >= 1000) {
+        $price_string = (string)$price_rounded;
+        $price_start = substr($price_string, 0, strlen($price_string) - 3);
         $price_end = substr($price_string, -3);
         $price_rounded = $price_start . ' ' . $price_end;
     }
 
     $price_full = $price_rounded . '<b class="rub">р</b>';
 
-    return  $price_full;
+    return $price_full;
 }
 
 
-function time_till_tomorrow_midnight(): string {
+function time_till_tomorrow_midnight(): string
+{
     $current_time = date_create('now');
     $tomorrow_midnight_time = date_create('tomorrow midnight');
     $diff_till_tomorrow_midnight = date_diff($tomorrow_midnight_time, $current_time);
 
-    return date_interval_format( $diff_till_tomorrow_midnight, "%H:%I" );
+    return date_interval_format($diff_till_tomorrow_midnight, "%H:%I");
 }
--- templates/index.php
+++ templates/index.php
@@ -1,36 +1,37 @@
+<section class="promo">
+    <h2 class="promo__title">Нужен стафф для катки?</h2>
+    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное
+        снаряжение.</p>
+    <ul class="promo__list">
 
-    <section class="promo">
-        <h2 class="promo__title">Нужен стафф для катки?</h2>
-        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
-        <ul class="promo__list">
-
-        <?php foreach ( $categories as $category ): ?>
+        <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars( $category ); ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($category); ?></a>
             </li>
         <?php endforeach; ?>
 
-        </ul>
-    </section>
-    <section class="lots">
-        <div class="lots__header">
-            <h2>Открытые лоты</h2>
-        </div>
-        <ul class="lots__list">
+    </ul>
+</section>
+<section class="lots">
+    <div class="lots__header">
+        <h2>Открытые лоты</h2>
+    </div>
+    <ul class="lots__list">
 
-        <?php foreach ( $lots as $lot ): ?>
+        <?php foreach ($lots as $lot): ?>
 
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= htmlspecialchars( $lot['img_url'] ); ?>" width="350" height="260" alt="">
+                    <img src="<?= htmlspecialchars($lot['img_url']); ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= htmlspecialchars( $lot['cat'] ); ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars( $lot['title'] ); ?></a></h3>
+                    <span class="lot__category"><?= htmlspecialchars($lot['cat']); ?></span>
+                    <h3 class="lot__title"><a class="text-link"
+                                              href="pages/lot.html"><?= htmlspecialchars($lot['title']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= format_price( $lot['price'] ); ?></span>
+                            <span class="lot__cost"><?= format_price($lot['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             <?= time_till_tomorrow_midnight(); ?>
@@ -41,5 +42,5 @@
 
         <?php endforeach; ?>
 
-        </ul>
-    </section>
+    </ul>
+</section>
